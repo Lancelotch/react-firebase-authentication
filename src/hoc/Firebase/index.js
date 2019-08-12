c220708de29071310b790dafb355bb885f283014
Candra Darmawan <chandra.souvenir@gmail.com>
2019-08-12 20:12:43 +0700

    Provide Firebase in react

---
+++ src/hoc/Firebase/index.js
@@ -0,0 +1,5 @@
+import Firebase from './config';
+import FireBaseContext from './Context';
+
+export default Firebase;
+export {FireBaseContext};
--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import Firebase, { FireBaseContext } from "./hoc/Firebase";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <FireBaseContext.Provider value={new Firebase()}>
+    <App />
+  </FireBaseContext.Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
